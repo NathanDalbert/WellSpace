ba021556028e2db794901767e8de1682e3680406
NathanDalbert <dalbert.nathan@gmail.com>
2025-03-28 17:30:08 -0300

    feat: criar usuarioRequest dto

--- src/main/java/com/WellSpace/modules/usuario/DTO/UsuarioRequest.java
+++ src/main/java/com/WellSpace/modules/usuario/DTO/UsuarioRequest.java
@@ -1,5 +1,5 @@
 package com.WellSpace.modules.usuario.DTO;
 
-public record UsuarioRequest( String nome, String email, String fotoPerfil, Boolean integridade, String dataNascimento) {
+public record UsuarioRequest( String nome, String email,String senha, String fotoPerfil, Boolean integridade, String dataNascimento) {
 
 }
