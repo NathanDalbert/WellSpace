db7b7b9781d6eefaad0097166f3edce339423fdf
NathanDalbert <dalbert.nathan@gmail.com>
2025-03-29 20:56:56 -0300

    refactor: ajustar swagger para comportar spring security

--- src/main/java/com/WellSpace/config/SwaggerConfig.java
+++ src/main/java/com/WellSpace/config/SwaggerConfig.java
@@ -1,22 +1,29 @@
 package com.WellSpace.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+
 
 @Configuration
+
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("WellSpace API")
-                        .description("API documentation for WellSpace application")
-                        .version("1.0.0")
-
-
-                );
+                .info(new Info().title("WellSpace API").version("1.0"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
