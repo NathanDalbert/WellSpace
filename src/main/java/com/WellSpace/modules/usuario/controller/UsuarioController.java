d396234cebd587122b048d870619240000239063
NathanDalbert <dalbert.nathan@gmail.com>
2025-03-29 20:57:17 -0300

    refactor: reomover metodo de criar usuario

--- src/main/java/com/WellSpace/modules/usuario/controller/UsuarioController.java
+++ src/main/java/com/WellSpace/modules/usuario/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import com.WellSpace.modules.usuario.DTO.UsuarioRequest;
 import com.WellSpace.modules.usuario.DTO.UsuarioResponse;
 import com.WellSpace.modules.usuario.DTO.UsuarioUpdateRequest;
 import com.WellSpace.modules.usuario.services.UsuarioService;
+
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,23 +15,15 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("usuario")
+@RequestMapping("/usuario")
 @AllArgsConstructor
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
 
-    // Criar usuário
-    @PostMapping("/criar")
-    public ResponseEntity<UsuarioResponse> criarUsuario(@Valid @RequestBody UsuarioRequest usuarioRequest, BindingResult result) {
-        // Verifica se há erros de validação
 
-        // Chama o serviço para criar o usuário
-        UsuarioResponse usuarioResponse = usuarioService.criarUsuario(usuarioRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioResponse);
-    }
 
-    // Buscar usuário por ID
+
     @PostMapping("/buscar")
     public ResponseEntity<UsuarioResponse> buscarUsuarioPorId(@Valid @RequestBody UUID usuarioId) {
         try {
@@ -42,23 +35,23 @@ public class UsuarioController {
         }
     }
 
-    // Atualizar usuário
+
     @PutMapping("/atualizar")
     public ResponseEntity<UsuarioResponse> atualizarUsuario(@RequestParam UUID usuarioId, @Valid @RequestBody UsuarioUpdateRequest usuarioUpdateRequest) {
 
 
         try {
-            // Chama o serviço para atualizar o usuário
+
             UsuarioResponse usuarioResponse = usuarioService.atualizarUsuario(usuarioId, usuarioUpdateRequest);
             return ResponseEntity.ok(usuarioResponse);
         } catch (RuntimeException e) {
-            // Caso o usuário não seja encontrado, retorna 404 com a mensagem de erro
+
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new UsuarioResponse(
                     null, "Erro", null, null, null, null));
         }
     }
 
-    // Deletar usuário
+
     @PostMapping("/deletar")
     public ResponseEntity<String> deletarUsuario(@Valid @RequestBody UUID usuarioId) {
         try {
