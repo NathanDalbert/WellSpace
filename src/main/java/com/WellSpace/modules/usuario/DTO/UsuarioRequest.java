7ac5297576e69ffc8bbb6c8de6235ea44985a410
NathanDalbert <dalbert.nathan@gmail.com>
2025-03-29 10:13:55 -0300

    feat: Criar usuarioRequest

--- src/main/java/com/WellSpace/modules/usuario/DTO/UsuarioRequest.java
+++ src/main/java/com/WellSpace/modules/usuario/DTO/UsuarioRequest.java
@@ -1,5 +1,30 @@
 package com.WellSpace.modules.usuario.DTO;
 
-public record UsuarioRequest( String nome, String email,String senha, String fotoPerfil, Boolean integridade, String dataNascimento) {
+import com.WellSpace.utils.validations.MaiorDe18;
+import jakarta.validation.constraints.*;
 
-}
+import java.time.LocalDate;
+
+public record UsuarioRequest(
+        @NotBlank(message = "O nome é obrigatório")
+        @Size(min = 10, message = "O nome deve ter no mínimo 10 caracteres")
+        String nome,
+
+        @NotBlank(message = "O e-mail é obrigatório")
+        @Email(message = "O e-mail deve ser válido")
+        String email,
+
+        @NotBlank(message = "A senha é obrigatória")
+        @Size(min = 8, message = "A senha deve ter no mínimo 8 caracteres")
+        String senha,
+
+        String fotoPerfil,
+
+        @NotNull(message = "A integridade é obrigatória")
+        Boolean integridade,
+
+        @NotNull(message = "A data de nascimento é obrigatória")
+        @PastOrPresent(message = "A data de nascimento não pode ser no futuro")
+        @MaiorDe18()
+        LocalDate dataNascimento
+) {}
