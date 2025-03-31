3bc7c9ee4499383d13a4e0d573c40170b9741b88
NathanDalbert <dalbert.nathan@gmail.com>
2025-03-31 11:42:57 -0300

    feat: adicionar respostas personalizadas do Swagger

--- src/main/java/com/WellSpace/modules/usuario/controller/UsuarioController.java
+++ src/main/java/com/WellSpace/modules/usuario/controller/UsuarioController.java
@@ -2,7 +2,11 @@ package com.WellSpace.modules.usuario.controller;
 
 import com.WellSpace.modules.usuario.DTO.UsuarioResponse;
 import com.WellSpace.modules.usuario.DTO.UsuarioUpdateRequest;
-import com.WellSpace.modules.usuario.services.UsuarioService;
+import com.WellSpace.modules.usuario.services.interfaces.UsuarioServiceInterface;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,17 +19,21 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/usuario")
 @AllArgsConstructor
+@Tag(name = "Usuario", description = "Operações relacionadas ao gerenciamento de usuários")
 public class UsuarioController {
 
-    private final UsuarioService usuarioService;
+    private final UsuarioServiceInterface usuarioService;
 
-    @PostMapping("/buscar")
+    @GetMapping("/buscar/{id}")
     @PreAuthorize("hasRole('ADMIN') or hasRole('LOCATARIO')")
-    public ResponseEntity<UsuarioResponse> buscarUsuarioPorId(@RequestBody @Valid UsuarioUpdateRequest usuarioUpdateRequest) {
+    @Operation(summary = "Buscar usuário por ID", description = "Busca um usuário pelo seu ID. Somente ADMIN e LOCATARIO podem acessar.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário encontrado"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+    })
+    public ResponseEntity<UsuarioResponse> buscarUsuarioPorId(@PathVariable UUID id) {
         try {
-
-            UUID usuarioId = usuarioUpdateRequest.usuarioId();
-            UsuarioResponse usuarioResponse = usuarioService.buscarUsuarioPorId(usuarioId);
+            UsuarioResponse usuarioResponse = usuarioService.buscarUsuarioPorId(id);
             return ResponseEntity.ok(usuarioResponse);
         } catch (RuntimeException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new UsuarioResponse(
@@ -33,9 +41,13 @@ public class UsuarioController {
         }
     }
 
-
     @PutMapping("/atualizar")
     @PreAuthorize("hasRole('ADMIN') or hasRole('LOCATARIO')")
+    @Operation(summary = "Atualizar dados do usuário", description = "Atualiza os dados de um usuário pelo seu ID. Somente ADMIN e LOCATARIO podem acessar.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário atualizado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+    })
     public ResponseEntity<UsuarioResponse> atualizarUsuario(@RequestBody @Valid UsuarioUpdateRequest usuarioUpdateRequest) {
         try {
             UUID usuarioId = usuarioUpdateRequest.usuarioId();
@@ -47,12 +59,16 @@ public class UsuarioController {
         }
     }
 
-    @DeleteMapping("/deletar")
+    @DeleteMapping("/deletar/{id}")
     @PreAuthorize("hasRole('ADMIN') or hasRole('LOCATARIO')")
-    public ResponseEntity<String> deletarUsuario(@RequestBody @Valid UsuarioUpdateRequest usuarioUpdateRequest) {
+    @Operation(summary = "Deletar usuário", description = "Deleta um usuário pelo seu ID. Somente ADMIN e LOCATARIO podem acessar.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Usuário deletado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+    })
+    public ResponseEntity<String> deletarUsuario(@PathVariable UUID id) {
         try {
-            UUID usuarioId = usuarioUpdateRequest.usuarioId();
-            usuarioService.deletarUsuario(usuarioId);
+            usuarioService.deletarUsuario(id);
             return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Usuário deletado com sucesso");
         } catch (RuntimeException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Erro ao deletar o usuário: " + e.getMessage());
