c93687a609df49a035fde2a0337a0e1e9794063e
NathanDalbert <dalbert.nathan@gmail.com>
2025-04-06 22:17:04 -0300

    feat: Criar exceções personalizadas para Salas

---
+++ src/main/java/com/WellSpace/modules/salas/execepitons/SalaNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package com.WellSpace.modules.salas.execepitons;
+
+public class SalaNaoEncontradaException     extends  RuntimeException {
+    public SalaNaoEncontradaException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/WellSpace/modules/salas/execepitons/TempoInvalidoException.java
@@ -0,0 +1,7 @@
+package com.WellSpace.modules.salas.execepitons;
+
+public class TempoInvalidoException extends RuntimeException {
+    public TempoInvalidoException(String message) {
+        super(message);
+    }
+}
