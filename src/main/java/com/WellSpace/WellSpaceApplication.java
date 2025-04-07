0d28c8a4004befd5a1f1cf54364528dd34c986b9
igoreor <igoreor24@gmail.com>
2025-04-07 15:48:36 -0300

    fix(main): ajustar a aplicação

--- src/main/java/com/WellSpace/WellSpaceApplication.java
+++ src/main/java/com/WellSpace/WellSpaceApplication.java
@@ -3,12 +3,10 @@ package com.WellSpace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class WellSpaceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WellSpaceApplication.class, args);
     }
-
 }
