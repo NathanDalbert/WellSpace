31f1000a0cb1272a1189d0696ff5071871c0cc6c
igoreor <igoreor24@gmail.com>
2025-04-08 11:47:37 -0300

    fix(SalasImages): ajustar interface de SalasImages

--- src/main/java/com/WellSpace/modules/salas_images/services/interfaces/SalasImagesServiceInterface.java
+++ src/main/java/com/WellSpace/modules/salas_images/services/interfaces/SalasImagesServiceInterface.java
@@ -1,4 +1,11 @@
 package com.WellSpace.modules.salas_images.services.interfaces;
 
+import java.util.UUID;
+
+import com.WellSpace.modules.salas_images.DTO.SalasImagesRequest;
+import com.WellSpace.modules.salas_images.domain.SalasImages;
+
 public interface SalasImagesServiceInterface {
+    public SalasImages salvarImagem(UUID salasId, SalasImagesRequest request);
+
 }
