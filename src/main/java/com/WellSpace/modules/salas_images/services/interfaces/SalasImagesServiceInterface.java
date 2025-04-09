25dc79e74696bbffc9af2dcac4f9a06fbb45ad67
igoreor <igoreor24@gmail.com>
2025-04-08 22:43:52 -0300

    refactor(salas-images): atualiza interface para refletir retorno com DTOs

--- src/main/java/com/WellSpace/modules/salas_images/services/interfaces/SalasImagesServiceInterface.java
+++ src/main/java/com/WellSpace/modules/salas_images/services/interfaces/SalasImagesServiceInterface.java
@@ -1,11 +1,22 @@
 package com.WellSpace.modules.salas_images.services.interfaces;
 
+import com.WellSpace.modules.salas_images.DTO.SalasImagesRequest;
+import com.WellSpace.modules.salas_images.DTO.SalasImagesResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
-import com.WellSpace.modules.salas_images.DTO.SalasImagesRequest;
-import com.WellSpace.modules.salas_images.domain.SalasImages;
-
 public interface SalasImagesServiceInterface {
-    public SalasImages salvarImagem(UUID salasId, SalasImagesRequest request);
 
+    SalasImagesResponse salvarImagem(UUID salasId, SalasImagesRequest request) throws IOException;
+
+    List<SalasImagesResponse> listarImagensPorSala(UUID salaId);
+
+    SalasImagesResponse buscarPorId(UUID id);
+
+    SalasImagesResponse atualizarImagem(UUID id, MultipartFile novaImagem) throws IOException;
+
+    void deletarImagem(UUID id);
 }
