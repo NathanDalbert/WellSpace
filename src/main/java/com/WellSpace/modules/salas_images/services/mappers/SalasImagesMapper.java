cf494c0827cdf772b3e8fbdca8e3ae16556bff7c
igoreor <igoreor24@gmail.com>
2025-04-08 22:44:02 -0300

    feat(salas-images): adiciona conversão entre entidade e DTO no mapper

--- src/main/java/com/WellSpace/modules/salas_images/services/mappers/SalasImagesMapper.java
+++ src/main/java/com/WellSpace/modules/salas_images/services/mappers/SalasImagesMapper.java
@@ -1,14 +1,21 @@
 package com.WellSpace.modules.salas_images.services.mappers;
 
+import com.WellSpace.modules.salas_images.DTO.SalasImagesResponse;
 import com.WellSpace.modules.salas_images.domain.SalasImages;
 import com.WellSpace.modules.salas.domain.Salas;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SalasImagesMapper {
 
-    public static SalasImages toEntity(String imageUrl, Salas sala) {
+    public SalasImages toEntity(String imageUrl, Salas sala) {
         SalasImages entity = new SalasImages();
         entity.setImagem(imageUrl);
         entity.setSala(sala);
         return entity;
+    }
+
+    public SalasImagesResponse toResponse(SalasImages entity) {
+        return new SalasImagesResponse(entity.getSalasImagesId(), entity.getImagem());
     }
 }
