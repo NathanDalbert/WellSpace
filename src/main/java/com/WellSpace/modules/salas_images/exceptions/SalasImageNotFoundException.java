9aab57a6cc6e147a57730fc5a56ba5f5caa5116c
igoreor <igoreor24@gmail.com>
2025-04-08 22:44:22 -0300

    feat(salas-images): adiciona exceção customizada para imagem não encontrada

---
+++ src/main/java/com/WellSpace/modules/salas_images/exceptions/SalasImageNotFoundException.java
@@ -0,0 +1,7 @@
+package com.WellSpace.modules.salas_images.exceptions;
+
+public class SalasImageNotFoundException extends RuntimeException {
+    public SalasImageNotFoundException(String message) {
+        super(message);
+    }
+}
