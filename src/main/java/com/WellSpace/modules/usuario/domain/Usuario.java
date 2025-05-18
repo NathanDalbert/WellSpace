af08fb188ebae8481f4785ecd5c00fc71e719f72
NathanDalbert <dalbert.nathan@gmail.com>
2025-05-18 20:07:06 -0300

    refactor(Usuario): Ajustar sistema de role para setar novo usario como LOCADOR

--- src/main/java/com/WellSpace/modules/usuario/domain/Usuario.java
+++ src/main/java/com/WellSpace/modules/usuario/domain/Usuario.java
@@ -9,9 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
 import java.time.LocalDate;
-
 import java.util.List;
 import java.util.UUID;
 
@@ -19,12 +17,13 @@ import java.util.UUID;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "usuario")
+@Entity
 @Table(name = "usuario")
-public class Usuario  {
+public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false)
     private UUID usuarioId;
 
     @Column(name = "nome", nullable = false)
@@ -36,36 +35,37 @@ public class Usuario  {
     @Column(name = "senha", nullable = false)
     private String senha;
 
-    @Column(name = "fotoPerfil", nullable = false)
+    @Column(name = "foto_perfil", nullable = false)
     private String fotoPerfil;
 
     @Column(name = "integridade", nullable = false)
     private Boolean integridade;
 
-    @Column(name = "dataNascimento", nullable = false)
+    @Column(name = "data_nascimento", nullable = false)
     private LocalDate dataNascimento;
+
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false, length = 50)
+    @Column(name = "user_role", nullable = false, length = 50)
     private UsuarioRole usuarioRole = UsuarioRole.LOCADOR;
 
     @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Contato> contatos;
 
     @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
-    List<Salas>   salas;
+    private List<Salas> salas;
 
-
-    public Usuario(String nome, String email, String senha, String fotoPerfil, Boolean integridade, LocalDate dataNascimento) {
+    private Usuario(String nome, String email, String senha, String fotoPerfil, Boolean integridade, LocalDate dataNascimento) {
         this.nome = nome;
         this.email = email;
         this.senha = senha;
         this.fotoPerfil = fotoPerfil;
         this.integridade = integridade;
         this.dataNascimento = dataNascimento;
+        this.usuarioRole = UsuarioRole.LOCADOR;
     }
 
     public static Usuario newUsuario(String nome, String email, String senha, String fotoPerfil, Boolean integridade, LocalDate dataNascimento) {
-        return new Usuario(nome, email, senha, fotoPerfil, integridade, dataNascimento);
+        Usuario usuario = new Usuario(nome, email, senha, fotoPerfil, integridade, dataNascimento);
+        return usuario;
     }
-    
 }
