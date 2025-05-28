a9d2bc4f00e65c999d30e09fd9ce1b9ebf41df5c
NathanDalbert <dalbert.nathan@gmail.com>
2025-05-28 16:52:51 -0300

    refactor(EmailService): adcionar injeção de dependencia por meio do RequiredArgsConstructor

--- src/main/java/com/WellSpace/modules/usuario/services/EmailService.java
+++ src/main/java/com/WellSpace/modules/usuario/services/EmailService.java
@@ -1,21 +1,24 @@
 package com.WellSpace.modules.usuario.services;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+@Transactional
+@RequiredArgsConstructor
 @Service
 public class EmailService {
 
     private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
 
-    @Autowired
-    private JavaMailSender mailSender;
+
+    private final JavaMailSender mailSender;
 
     @Value("${spring.mail.username}")
     private String fromEmail;
