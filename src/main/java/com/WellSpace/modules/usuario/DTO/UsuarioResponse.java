b6de7ea5efd11dc7700c1fff123ab3b5332e9936
NathanDalbert <dalbert.nathan@gmail.com>
2025-06-18 22:27:34 -0300

    fix(dto/usuario): diciona `usuarioRole` ao `UsuarioResponse`

--- src/main/java/com/WellSpace/modules/usuario/DTO/UsuarioResponse.java
+++ src/main/java/com/WellSpace/modules/usuario/DTO/UsuarioResponse.java
@@ -1,5 +1,6 @@
 package com.WellSpace.modules.usuario.DTO;
 
+import com.WellSpace.modules.usuario.domain.ENUM.UsuarioRole; // Importar UsuarioRole
 import java.time.LocalDate;
 import java.util.Optional;
 import java.util.UUID;
@@ -10,5 +11,6 @@ public record UsuarioResponse(
         String email,
         Optional<String> fotoPerfil,
         Optional<Boolean> integridade,
-        Optional<LocalDate> dataNascimento
-     ) {}
+        Optional<LocalDate> dataNascimento,
+        UsuarioRole usuarioRole
+) {}
