f30912380dcde6ef61a7d4f05fe44d3ecb4beefa
igoreor <igoreor24@gmail.com>
2025-06-19 20:16:58 -0300

    feat(Migration): criar tabela de favoritos

--- src/main/resources/db/migration/V7__create_table_favoritos.sql
+++ src/main/resources/db/migration/V7__create_table_favoritos.sql
@@ -1,1 +1,8 @@
-DROP TABLE IF EXISTS favoritos CASCADE;
+CREATE TABLE favoritos (
+    favorito_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    usuario_id UUID NOT NULL,
+    sala_id UUID NOT NULL,
+    CONSTRAINT fk_favorito_usuario FOREIGN KEY (usuario_id) REFERENCES usuario(id) ON DELETE CASCADE,
+    CONSTRAINT fk_favorito_sala FOREIGN KEY (sala_id) REFERENCES salas(salas_id) ON DELETE CASCADE,
+    CONSTRAINT unique_favorito UNIQUE (usuario_id, sala_id)
+);
