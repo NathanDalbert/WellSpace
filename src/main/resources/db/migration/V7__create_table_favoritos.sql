523dd97ba60bd04dc9f2b40208364dc067d98cd1
igoreor <igoreor24@gmail.com>
2025-06-19 20:02:48 -0300

    fix(FavoritosTable): apagar tabela

--- src/main/resources/db/migration/V7__create_table_favoritos.sql
+++ src/main/resources/db/migration/V7__create_table_favoritos.sql
@@ -1,8 +1,1 @@
-CREATE TABLE favoritos (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    usuario_id UUID NOT NULL,
-    sala_id UUID NOT NULL,
-    CONSTRAINT fk_favorito_usuario FOREIGN KEY (usuario_id) REFERENCES usuario(id) ON DELETE CASCADE,
-    CONSTRAINT fk_favorito_sala FOREIGN KEY (sala_id) REFERENCES salas(salas_id) ON DELETE CASCADE,
-    CONSTRAINT unique_favorito UNIQUE (usuario_id, sala_id)
-);
+DROP TABLE IF EXISTS favoritos CASCADE;
